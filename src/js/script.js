5ee7969148e7b3b4e2b39a0f407e063feb280719
dpres <thepressu@gmail.com>
2018-07-20 20:33:32 +0200

    toTop button

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,18 @@ $(document).ready(function () {
         scrollOffset: 160,
     });
 });
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var showButtonHeight = 160;
+    if (document.body.scrollTop > showButtonHeight || document.documentElement.scrollTop > showButtonHeight) {
+        $('#toTop').addClass('show');
+    } else {
+        $('#toTop').removeClass('show');
+    }
+}
